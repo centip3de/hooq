a1a08365c90c3342e86961474efca70d8cf5db99
Forest Belton <forest@homolo.gy>
2016-09-06 23:19:51 -0700

    Be more precise in table query

--- src/Hooq/Postgres.hs
+++ src/Hooq/Postgres.hs
@@ -65,7 +65,7 @@ getTableQuery = appendNull $ C.concat
     [ " SELECT c.oid, n.nspname, c.relname"
     , " FROM pg_catalog.pg_class c"
     , " LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace"
-    , " WHERE c.relname ~ '^(quotes)$'"
+    , " WHERE c.relname = 'quotes'"
     , " AND pg_catalog.pg_table_is_visible(c.oid) ORDER BY 2, 3;"
     ]
 
